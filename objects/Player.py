608e89b4c12ab6867859283f4d8843a8696ee008
BuildTools <unconfigured@null.spigotmc.org>
2020-08-13 18:39:59 -0500

    Still adding player object

---
+++ objects/Player.py
@@ -0,0 +1,15 @@
+class Player:
+    def __init__(self, name, team):
+        self.name = name
+        self.team = team
+        self.link = None
+        self.stats_2019 = {}
+        self.stats_2020 = {}
+
+    def update2020(self):
+        pass
+
+    def getLink(self):
+        pass
+    
+
